dca621c8a8939e81fcb7be9c45548c4f6d9390db
dezoliveira <andresoliveira@protonmail.com>
2021-07-31 15:26:08 -0300

    Adicionado estilo no jogo

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ let box = 32
 /* Criando vetor da cobrinha */
 let snake = []
 snake[0] = {
-  x: 8 * box,
-  y: 8 * box
+  x: 1 * box,
+  y: 1 * box
 }
 
 /* Movimentar a cobrinha */
@@ -26,7 +26,9 @@ let food = {
 
 /* Criar o background */
 function criarBG(){
-  context.fillStyle = "lightgreen"
+  context.fillStyle = "#A1C192"
+
+  //desenha o retângulo usando x e y e a largura e altura setadas
   context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
@@ -35,14 +37,14 @@ function criarCobrinha(){
   for(i=0; i < snake.length; i++){
     /* Cor da cobrinha */
     /* Tamanho da cobrinha (inicial até o final (tamanho do cenário)) */
-    context.fillStyle = "green"
+    context.fillStyle = "#4A5844"
     context.fillRect(snake[i].x, snake[i].y, box, box)
   }
 }
 
 /* Funçao para desenhar a comida */
 function drawFood(){
-  context.fillStyle = "red"
+  context.fillStyle = "#4A5844"
   context.fillRect(food.x, food.y, box, box)
 }
 
@@ -71,7 +73,7 @@ function iniciarJogo(){
     if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
       clearInterval(jogo)
       alert('Game Over :(')
-      clearInterval(jogo)
+      location.reload()
     }
   }
 
@@ -88,13 +90,13 @@ function iniciarJogo(){
   if(direction == "up") snakeY -= box
   if(direction == "down") snakeY += box
 
-  /* Função para aumentar ou diminuir a cobrinha */
+  /* Função para aumentar a cobrinha */
   if(snakeX != food.x || snakeY != food.y){
-    /* Retira o ultiom elemento da cobrinha */
+    /* Acrescenta a comida na cobrinha */
     snake.pop()
   }
   else{
-    /* Adiciona a comida no estomago da cobrinha */
+    /* Adiciona a comida em outro local do canvas */
     food.x = Math.floor(Math.random() * 15 + 1) * box,
     food.y = Math.floor(Math.random() * 15 + 1) * box
   }
@@ -105,6 +107,7 @@ function iniciarJogo(){
     y: snakeY
   }
 
+  //método unshift adiciona como primeiro quadradinho da cobrinha
   snake.unshift(newHead)
 }
 
